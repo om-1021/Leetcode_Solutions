fe7df1b3232711b2513741bbde69b4cd760fe876
Om Dubey <omdubey1021@gmail.com>
2024-07-03 17:05:07 +0530

    Time: 550 ms (80.27%), Space: 0B (100%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.sql
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-Select tweet_id from Tweets where Length(content) >15;
+-- Select tweet_id from Tweets where Length(content) >15;
+select tweet_id from tweets where char_length(content) > 15;
