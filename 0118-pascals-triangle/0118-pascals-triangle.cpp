dbe5b88a15b15f893a0746df470e828cca4e2fdf
Om Dubey <omdubey1021@gmail.com>
2024-07-09 17:49:18 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.77%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -15,8 +15,7 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
         for(int i=1;i<=numRows;i++){
-            vector<int> temp = generateRow(i);
-            ans.push_back(temp);
+            ans.push_back(generateRow(i));
         }
 
         return ans;
