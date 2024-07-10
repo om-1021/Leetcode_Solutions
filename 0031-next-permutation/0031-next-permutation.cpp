a579758208fe3af9d5d330543ddd39dcc4375899
Om Dubey <omdubey1021@gmail.com>
2024-07-10 17:44:50 +0530

    Time: 2 ms (67.55%), Space: 14.5 MB (54.76%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,12 +5,16 @@ public:
         // next_permutation(nums.begin(),nums.end());
         int index = -1;
         int n = nums.size();
+
+        // finding the break point where the left elements is smaller then the right element
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 index = i;
                 break;
             }
         }
+
+        // swapping the next greater element from the breakpoint element
         if (index != -1) {
             for (int i = n - 1; i >= index; i--) {
                 if (nums[i] > nums[index]) {
@@ -18,10 +22,12 @@ public:
                     break;
                 }
             }
-
+        // sorting the remaining the array to get the minimum value 
         sort(nums.begin()+index+1,nums.end());
         }
         else
+        // if no break point is found that means its the largest permutation and next permutation will be the smallest one
+        // which will be the reverse of the largest one 
         reverse(nums.begin(),nums.end());
 
     }
