af28f2270d9baa84e1a6dde47c66ef63553be6c2
Om Dubey <omdubey1021@gmail.com>
2024-07-12 14:25:38 +0530

    Time: 0 ms (100%), Space: 10.1 MB (7.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int index0 = 0;
-        int index2 = n - 1;
-        
-        for(int i=0;i<n;i++){
-            if(nums[i] == 0){
-                swap(nums[i],nums[index0]);
-                index0++;
+        int i = 0;
+        int j = n - 1;
+        int itr = 0;
+       
+       // Dutch National Flag Algorithm
+       // take 3 pointers 1 for keeping track of index for 0
+       // 2nd for keeping track of index for 2 
+       // 3rd for iteratiing over the array
+       // whenever 0 is encountered place it at index i - > increase both iterator and i pointers
+       // whenever 2 is encountered place it at index -> decrement only j pointer
+        while (itr <= j) {
+            if (nums[itr] == 1) {
+                itr++;
+                continue;
+               }
+            else if (nums[itr] == 0) {
+                swap(nums[itr], nums[i]);
+                itr++;
+                i++;
             }
-        }
 
-        for(int i=n-1;i>=0;i--){
-            if(nums[i] == 2){
-                swap(nums[i],nums[index2]);
-                index2--;
+           else if (nums[itr] == 2) {
+                swap(nums[itr], nums[j]);
+                j--;
             }
+
+            
         }
     }
 };
