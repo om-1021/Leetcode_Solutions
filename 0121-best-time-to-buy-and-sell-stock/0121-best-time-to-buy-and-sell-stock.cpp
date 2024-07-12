d0dde4b72e875bcf7d410c6fb65b5c5c594e1ada
Om Dubey <omdubey1021@gmail.com>
2024-07-12 15:45:07 +0530

    Time: 88 ms (48.68%), Space: 96.2 MB (6.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,6 +3,7 @@ public:
     int maxProfit(vector<int>& prices) {
        // take a variable mini which will store the minimum element
        // take a variable max_profit which will store the max profit
+       // calculate the max_profit using current element and mini element
         int mini = INT_MAX;
         int max_profit = 0;
         for(int i=0;i<prices.size();i++){
