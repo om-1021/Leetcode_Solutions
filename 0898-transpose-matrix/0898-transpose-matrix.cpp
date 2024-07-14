308098fc0d7bd794875a553cafce94aa710d7fcb
Om Dubey <omdubey1021@gmail.com>
2024-07-14 13:13:58 +0530

    Time: 9 ms (59.23%), Space: 13.6 MB (9.07%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.cpp
+++ 0898-transpose-matrix/0898-transpose-matrix.cpp
@@ -3,6 +3,14 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
        int rows = matrix.size();
        int cols = matrix[0].size();
+       if( rows == cols){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<i;j++)
+            swap(matrix[i][j],matrix[j][i]);
+
+        }
+        return matrix;
+       }
 
        vector<vector<int>> result(cols,vector<int>(rows));
        for(int i=0;i<rows;i++){
