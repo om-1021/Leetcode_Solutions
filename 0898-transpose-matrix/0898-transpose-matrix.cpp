3fd85802b109ce998a7bcc1d2bb12ce3c7377bed
Om Dubey <omdubey1021@gmail.com>
2024-07-14 12:35:39 +0530

    Time: 9 ms (59.23%), Space: 13.6 MB (9.07%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.cpp
+++ 0898-transpose-matrix/0898-transpose-matrix.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> result(m, vector<int>(n));  // Create a new matrix with transposed dimensions
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                result[j][i] = matrix[i][j];
-            }
+       int rows = matrix.size();
+       int cols = matrix[0].size();
+
+       vector<vector<int>> result(cols,vector<int>(rows));
+       for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            result[j][i] = matrix[i][j];
         }
-        
-        return result;
+       }
+       return result;
     }
 };
