4775e32e16f6d3a2579114773d2a3d7791c6feb0
Om Dubey <omdubey1021@gmail.com>
2024-07-15 04:28:08 +0530

    Time: 0 ms (100%), Space: 9 MB (6.27%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -7,11 +7,15 @@ public:
         // step 2: just reverse each row of the matrix i -> n
         // reverse[arr[i].begin(),arr[i].end()]
         int n = matrix.size();
+        int elements = matrix.size() * matrix[0].size();
+        int count = 0;
 
         // transposing the matrix
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++)
-                swap(matrix[i][j], matrix[j][i]);
+            if (count < elements / 2) {
+                for (int j = 0; j < i; j++)
+                    swap(matrix[i][j], matrix[j][i]);
+            }
         }
 
         // reversing the rows
