f0b54bc78dd63a107487f5b54abeb51e5a8742d7
Om Dubey <omdubey1021@gmail.com>
2024-07-15 06:16:45 +0530

    Time: 12 ms (98.63%), Space: 22.9 MB (29.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,20 +6,35 @@ public:
         vector<vector<int>> ans;
         sort(arr.begin(), arr.end());
 
-        for (int i = 0; i < n; i++) {
-            int start = arr[i][0];
-            int end = arr[i][1];
+        // for (int i = 0; i < n; i++) {
+        //     int start = arr[i][0];
+        //     int end = arr[i][1];
 
-            if (!ans.empty() && end <= ans.back()[1])
+        //     if (!ans.empty() && end <= ans.back()[1])
+        //         continue;
+
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (arr[j][0] <= end)
+        //             end = max(arr[j][1], end);
+        //         else break;
+        //     }
+        //     ans.push_back({start, end});
+        // }
+        ans.push_back(arr[0]);
+        for(int i =1;i<n;i++){
+            if(ans.back()[1] >= arr[i][1])
+            continue;
+            if(arr[i][0] <= ans.back()[1]){
+                ans.back()[1] = max(arr[i][1],ans.back()[1]);
                 continue;
-
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j][0] <= end)
-                    end = max(arr[j][1], end);
-                else break;
             }
-            ans.push_back({start, end});
+            ans.push_back({arr[i][0],arr[i][1]});
+
         }
+
+
+
+
         return ans;
     }
 };
