75f5bf46913fd3300b79e913cc138b2c28e89cbf
Om Dubey <omdubey1021@gmail.com>
2024-07-25 13:02:29 +0530

    Time: 0 ms (100%), Space: 7.5 MB (11.88%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -4,7 +4,7 @@ public:
         if(n==1,k==1)
         return 0;
 
-        int mid = pow(2,n-1) /2;
+        int mid = pow(2,n-2);
         if(k<=mid){
             return kthGrammar(n-1,k);
         }
