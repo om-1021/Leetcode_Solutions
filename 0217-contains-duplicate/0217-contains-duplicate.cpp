d9bbd8a7fa2f51c77cdb02625fbdb34d15db29fd
Om Dubey <omdubey1021@gmail.com>
2024-08-17 08:40:16 +0530

    Time: 101 ms (36.22%), Space: 75.6 MB (14.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+    unordered_set<int> s;
+        // for(int i=0; i<nums.size();i++)
+        // {
+        //     if(s.find(nums[i])==s.end())
+        //     {
+        //         s.insert(nums[i]);
+        //     }
+        //     else 
+        //         return true;
+        // }
+        s.insert(nums.begin(),nums.end());
+        if(s.size() == nums.size())
+        return false;
+        else
+        return true;
+        
+    }
+};
