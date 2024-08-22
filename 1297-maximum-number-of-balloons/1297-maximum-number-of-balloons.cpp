8d02b1e1e0d61d04b8a6a85e9bf6dd6ab5a4fbf5
Om Dubey <omdubey1021@gmail.com>
2024-08-22 14:45:26 +0530

    Time: 6 ms (27.99%), Space: 9.3 MB (36.74%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+      unordered_map<char,int> umap;
+      for(auto x : text){
+        umap[x]++;
+      }
+      umap['l'] /= 2;
+      umap['o'] /= 2;
+      int count = 0;
+
+      while( umap['b'] >0 and  umap['a']>0 and  umap['l'] >0 and  umap['o']>0 and umap['n'] > 0){
+        umap['b']--;
+        umap['a']--;
+        umap['l']--;
+        umap['o']--;
+        umap['n']--;
+        count++;
+      }
+      return count;
+
+
+        
+    }
+};
