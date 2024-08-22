049f3d160bafb21e0aa938031c9cdb4d7b88b569
Om Dubey <omdubey1021@gmail.com>
2024-08-22 14:51:09 +0530

    Time: 3 ms (68.25%), Space: 9.3 MB (36.74%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-      unordered_map<char,int> umap;
-      for(auto x : text){
-        umap[x]++;
-      }
-      umap['l'] /= 2;
-      umap['o'] /= 2;
-      int count = 0;
+        unordered_map<char, int> umap;
+        for (auto x : text) {
+            umap[x]++;
+        }
+          umap['l'] /= 2;
+          umap['o'] /= 2;
+        int count = 0;
 
-      while( umap['b'] >0 and  umap['a']>0 and  umap['l'] >0 and  umap['o']>0 and umap['n'] > 0){
-        umap['b']--;
-        umap['a']--;
-        umap['l']--;
-        umap['o']--;
-        umap['n']--;
-        count++;
-      }
-      return count;
-
-
-        
+        // while (umap['b'] > 0 and umap['a'] > 0 and umap['l'] > 0 and
+        //        umap['o'] > 0 and umap['n'] > 0) {
+        //     umap['b']--;
+        //     umap['a']--;
+        //     umap['l']--;
+        //     umap['o']--;
+        //     umap['n']--;
+        //     count++;
+        // }
+        return min({umap['b'], umap['a'], umap['l'], umap['o'], umap['n']});
     }
 };
