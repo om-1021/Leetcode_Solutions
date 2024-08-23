fbef80e75a84004c8bb8c12c255b1810fac93be8
Om Dubey <omdubey1021@gmail.com>
2024-08-24 01:34:55 +0530

    Time: 10 ms (60.56%), Space: 14.6 MB (59.96%) - LeetHub

--- 1547-destination-city/1547-destination-city.cpp
+++ 1547-destination-city/1547-destination-city.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,string> umap;
 
+        
+        unordered_map<string,string> umap;
+        string ans ;
         for(int i=0;i<paths.size();i++){
             umap[paths[i][0]]=paths[0][1];
         }
          for(int i=0;i<paths.size();i++){
             if(umap.find(paths[i][1]) == umap.end()){
-                return paths[i][1];
+                ans= paths[i][1];
+                break;
             }
         }
 
-    return "none";
+    return ans;
     }
 };
