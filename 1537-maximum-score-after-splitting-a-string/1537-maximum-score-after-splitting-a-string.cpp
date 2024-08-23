e92d8bf18a36ad33053f5136b7b67522ff65ac38
Om Dubey <omdubey1021@gmail.com>
2024-08-24 02:24:57 +0530

    Time: 4 ms (28.81%), Space: 8.1 MB (15.73%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -3,10 +3,10 @@ public:
     int maxScore(string s) {
         int n = s.size();
         vector<int> prefixSum1(n);
-        prefixSum1[n-1] = (int)(s[n-1]) - 48;
-        int sums1 = (int)s[n-1] - 48 ;
+        prefixSum1[n-1] = (int)(s[n-1]) - '0';
+        int sums1 = (int)s[n-1] - '0' ;
         for(int i=n-2;i>=0;i--){
-            sums1 += (int)s[i] - 48;
+            sums1 += (int)s[i] - '0';
             prefixSum1[i] = sums1 ;
         }
         int sums0 = 0;
