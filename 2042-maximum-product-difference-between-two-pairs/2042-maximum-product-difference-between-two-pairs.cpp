ea583788adcab90368056700dc4dbd176eac679e
Om Dubey <omdubey1021@gmail.com>
2024-08-24 01:43:42 +0530

    Time: 15 ms (90.05%), Space: 24.1 MB (14.08%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
@@ -4,6 +4,10 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
 
+        // find max-1 find max-2
+        // find min-1 find min-2 
+        // do it in O(n) thats it no need to sort
+
         return (nums[n-1]*nums[n-2]) - (nums[0] * nums[1]);
     }
 };
