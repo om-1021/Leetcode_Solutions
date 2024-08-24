ffe7d61574a91dbac828392360bd54f849337897
Om Dubey <omdubey1021@gmail.com>
2024-08-24 10:56:43 +0530

    Time: 474 ms (5.06%), Space: 24 MB (74.1%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,46 +1,45 @@
-// class Solution {
-// public:
-//     vector<int> findErrorNums(vector<int>& nums) {
-//          int dupeNum ;
-//          int actual_sum =0;
-//         //  int firstIndexOfDupe;
-//          for(int i=0;i<nums.size()-1;i++)
-//          for(int j= i+1;j<nums.size();j++){
-//             actual_sum += nums[j];
-//             if(nums[i] == nums[j]){
-//                 // firstIndexOfDupe = i;
-//             dupeNum = nums[i];
-//             }
-//          }
-
-//          int n = nums.size();
-//          int expected_sum = (int)(n*(n+1))/2;
-//          int missingNum = expected_sum - (actual_sum - dupeNum);
-
-//          return {dupeNum,missingNum};
-
-//     }
-// };
-
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int dupeNum = -1;
-        int actual_sum = 0;
-        int n = nums.size();
-        unordered_set<int> seen; // To track the seen numbers
-
-        for (int num : nums) {
-            actual_sum += num;
-            if (seen.find(num) != seen.end()) {
-                dupeNum = num; // Found the duplicate number
+         int dupeNum ;
+         int n = nums.size();
+         int actual_sum = nums[n-1];
+         for(int i=0;i<n-1;i++){
+            actual_sum += nums[i];
+         for(int j= i+1;j<n;j++){
+            if(nums[i] == nums[j]){
+            dupeNum = nums[i];
             }
-            seen.insert(num);
-        }
+         }
+         }
 
-        int expected_sum = n * (n + 1) / 2;
-        int missingNum = expected_sum - (actual_sum - dupeNum);
+         int expected_sum = n*(n+1)/2;
+         int missingNum = expected_sum - (actual_sum - dupeNum);
 
-        return {dupeNum, missingNum};
+         return {dupeNum,missingNum};
+
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findErrorNums(vector<int>& nums) {
+//         int dupeNum = -1;
+//         int actual_sum = 0;
+//         int n = nums.size();
+//         unordered_set<int> seen; // To track the seen numbers
+
+//         for (int num : nums) {
+//             actual_sum += num;
+//             if (seen.find(num) != seen.end()) {
+//                 dupeNum = num; // Found the duplicate number
+//             }
+//             seen.insert(num);
+//         }
+
+//         int expected_sum = n * (n + 1) / 2;
+//         int missingNum = expected_sum - (actual_sum - dupeNum);
+
+//         return {dupeNum, missingNum};
+//     }
+// };
