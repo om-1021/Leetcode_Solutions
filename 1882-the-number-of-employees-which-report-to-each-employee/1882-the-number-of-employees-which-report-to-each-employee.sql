299174cefcade3a45bb0c20d9860bb600a52b9a9
Om Dubey <omdubey1021@gmail.com>
2024-08-26 14:14:33 +0530

    Time: 781 ms (19.01%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -2,7 +2,7 @@
 SELECT  
     e1.employee_id,
     e1.name,
-    COUNT(e2.employee_id) as reports_count,
+    COUNT(e2.reports_to) as reports_count,
     ROUND(AVG(e2.age)) AS average_age
 FROM Employees AS e1 INNER JOIN Employees AS e2
 ON e1.employee_id = e2.reports_to
