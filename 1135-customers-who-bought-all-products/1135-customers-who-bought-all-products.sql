b4b811b2252cc842bc496afa501e2bbe6209bfeb
Om Dubey <omdubey1021@gmail.com>
2024-08-26 13:52:29 +0530

    Time: 586 ms (42.03%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -3,5 +3,5 @@ SELECT customer_id
 FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (
-    SELECT COUNT(DISTINCT product_key) FROM PRODUCT
+    SELECT COUNT(product_key) FROM PRODUCT
 )
