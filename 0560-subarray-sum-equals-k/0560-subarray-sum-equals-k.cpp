a1ea4f5ed3eaafc4dc52298952a5d648ae8ab942
Om Dubey <omdubey1021@gmail.com>
2024-08-29 13:25:29 +0530

    Time: 72 ms (59.7%), Space: 45.4 MB (61.29%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> umap;
-
+        // make a hash map which stores 
+        //all the available sums and thier count on the left side of the pointer
+        // then we check if there's and subarray with sum-k so that we can subtract
+        // and get the desired value k if a such subarray exists then how many 
+        // and we count the number of such subarrays using the count available in map
         int sum =0;
         int count =0;
         umap.insert({0,1});
