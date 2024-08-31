61c836c92bbdcc279884e87682f73fd46fb43b14
Om Dubey <omdubey1021@gmail.com>
2024-08-31 18:55:05 +0530

    Time: 298 ms (50.51%), Space: 15.7 MB (64.57%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+
+        unordered_set<char> letters;
+        for (int i = 0; i < s.length(); i++)
+            letters.insert(s[i]);
+
+        int count = 0;
+
+        for (char x : letters) {
+            int left_idx = -1;
+            int right_idx = -1;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == x) {
+                    if (left_idx == -1) {
+                        left_idx = i;
+                    }
+                    right_idx = i;
+                }
+            }
+
+                unordered_set<char> temp;
+                for (int j = left_idx + 1; j < right_idx; j++) {
+                    temp.insert(s[j]);
+                }
+
+                count += temp.size();
+            
+        }
+        return count;
+    }
+};
