5719aa71186be537cb0ba0c6f97a692aeecc3789
Om Dubey <omdubey1021@gmail.com>
2024-09-02 00:48:39 +0530

    Time: 33 ms (24.91%), Space: 14.6 MB (24.94%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+        if(p.length() > s.length()){
+            return {};
+        }
+
+        unordered_map<char,int> pCount;
+        unordered_map<char,int> sCount;
+        vector<int> result;
+        
+        for(int i=0;i<p.length();i++){
+            pCount[p[i]]++;
+            sCount[s[i]]++;
+        }
+        if(pCount == sCount)
+        result.push_back(0);
+
+        int left = 0;
+        for(int right = p.length(); right<s.length();right++){
+            sCount[s[right]]++;
+            sCount[s[left]]--;
+
+            if(sCount[s[left]] == 0 ){
+                sCount.erase(s[left]);
+            }
+            left++;
+            if(sCount == pCount){
+                result.push_back(left);
+            }
+
+        }
+        return result;
+
+
+
+        // max p map size = number of unique char in p at worst 26 abcdefghijklmnopqrstuvwxyz
+        //so TC: O(26 * n) or O(n)
+
+        
+        
+    }
+};
