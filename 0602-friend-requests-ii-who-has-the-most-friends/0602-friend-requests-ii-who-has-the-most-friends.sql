dda6bd78af1d00440b2850880042e1db01585160
Om Dubey <omdubey1021@gmail.com>
2024-09-03 16:58:33 +0530

    Time: 343 ms (21.67%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+SELECT id,COUNT(*) AS num
+    
+  FROM
+      (  
+         SELECT requester_id AS id FROM RequestAccepted
+
+        UNION ALL # it gives duplicates also
+
+        SELECT accepter_id AS id FROM RequestAccepted
+       ) 
+    AS friend_count
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
+   
