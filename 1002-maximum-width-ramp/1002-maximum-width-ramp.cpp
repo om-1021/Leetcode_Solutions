243b8733ee623181988c8ff2626c6f5656ea6d1c
Om Dubey <omdubey1021@gmail.com>
2024-10-10 20:32:28 +0530

    Time: 48 ms (51.63%), Space: 47.3 MB (16.92%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-         int n = nums.size();
+        int n = nums.size();
         vector<int> maxi = nums;
-
+        // keeps tracks of greatest elements on right
         for (int i = n - 2; i >= 0; i--) {
             maxi[i] = max(nums[i], maxi[i + 1]);
         }
 
         int left = 0;
         int res = 0;
+
         for (int right = 0; right < nums.size(); right++) {
-            while (nums[left] > maxi[right])
+            // if there are no greater element on the right then
+            // wont be able to create a window so move left pointer
+            // if left > right means no possible ramps are left
+
+            while (nums[left] > maxi[right]) {
+                if (left >= right)
+                    return 0;
                 left++;
+            }
 
             res = max(res, right - left);
         }
         return res;
-        
     }
 };
